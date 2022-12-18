ae63a4de763eba5c0ee2fc3f6116059c9fa58583
rkarambali <rkarambali@avaya.com>
2022-12-18 20:31:17 +0530

    ok

---
+++ JavaConfigExample/src/com/onesolvent/config/SportConfig.java
@@ -0,0 +1,31 @@
+package com.onesolvent.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import com.onesolvent.Coach;
+import com.onesolvent.TennisCoach;
+import com.onesolvent.service.FOrtuneServiceImpl2;
+import com.onesolvent.service.FortuneService;
+import com.onesolvent.service.FortuneServiceImpl;
+
+@Configuration
+@PropertySource("classpath:myprop.properties")
+@ComponentScan("com.onesolvent")
+public class SportConfig {
+	
+	
+	/*@Bean
+	public FortuneService getFortuneBean(){
+		return new FOrtuneServiceImpl2();
+	}
+	
+	@Bean
+	public Coach getCoachBean(){
+		return new TennisCoach(getFortuneBean());
+	}
+	*/
+
+}
