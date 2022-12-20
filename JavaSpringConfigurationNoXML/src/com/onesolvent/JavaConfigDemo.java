f543c3e06a69e9e674783c343aa0c58636f0fa40
rkarambali <rkarambali@avaya.com>
2022-12-20 21:26:45 +0530

    ok

---
+++ JavaSpringConfigurationNoXML/src/com/onesolvent/JavaConfigDemo.java
@@ -0,0 +1,13 @@
+package com.onesolvent;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class JavaConfigDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		AnnotationConfigApplicationContext context=new AnnotationConfigApplicationContext(SpotConfig.class);
+
+	}
+
+}
